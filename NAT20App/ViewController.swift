b8ccc42e84c7e8f53e6c407537948f0c9309d802
thegabbythemaster <gabegabby99@gmail.com>
2021-09-16 15:30:56 -0400

    Initial Commit

---
+++ NAT20App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NAT20App
+//
+//  Created by Gabby Gonzalez on 9/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
