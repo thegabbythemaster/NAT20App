c4a9e4b31fc66205a0bf127105a583b54a53d690
thegabbythemaster <gabegabby99@gmail.com>
2021-09-17 14:30:36 -0400

    added assets and logic to generate dice

--- NAT20App/ViewController.swift
+++ NAT20App/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var diceImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonAction(_ sender: Any) {
+        //generate random number between 1-6
+        let rolledNumber = Int.random(in: 1...6)
+        
+        //show the image for the dice in imageview according to rolled number
+        diceImage.image = UIImage(named: "d6-\(rolledNumber)")
+        
+        //roll animation
+        UIView.transition(with: diceImage, duration: 0.5, options: .transitionCurlDown, animations: nil, completion: nil)
+    }
+    
 }
 
