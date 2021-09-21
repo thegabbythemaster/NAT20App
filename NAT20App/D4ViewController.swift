913c23ec7f00da08557440a0f8a13f04c43a3178
thegabbythemaster <gabegabby99@gmail.com>
2021-09-21 19:05:03 -0400

    start implementation for d4,d8

---
+++ NAT20App/D4ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  D4ViewController.swift
+//  NAT20App
+//
+//  Created by Gabby Gonzalez on 9/21/21.
+//
+
+import UIKit
+
+class D4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- NAT20App/ViewController.swift
+++ NAT20App/ViewController.swift
@@ -8,35 +8,65 @@
 import UIKit
 import SideMenu
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var diceImage: UIImageView!
+class ViewController: UIViewController, MenuControllerDelegate {
     
-    var menu: SideMenuNavigationController?
+    @IBOutlet weak var diceImage: UIImageView!
+    @IBOutlet weak var diceLabel: UILabel!
+    
+    private var sideMenu: SideMenuNavigationController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        menu = SideMenuNavigationController(rootViewController: UIViewController())
+        let menu = MenuListController(with: ["D4", "D6", "D8","D10", "D%10", "D12", "D20"])
+        menu.delegate = self
         
-        let rolledNumber = Int.random(in: 1...6)
-        diceImage.image = UIImage(named: "d6-\(rolledNumber)")
+        sideMenu = SideMenuNavigationController(rootViewController: menu)
+        sideMenu?.leftSide = true
         
+        sideMenu?.setNavigationBarHidden(true, animated: false)
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapGeasture))
-        
         diceImage.addGestureRecognizer(tapGesture)
     }
     @IBAction func didTapMenu (){
-        present(menu!, animated: true)
+        present(sideMenu!, animated: true)
+    }
+    
+    func didSelectMenuItem(named: String) {
+        sideMenu?.dismiss(animated: true, completion: { [weak self] in
+            if named == "D4" {
+                self?.diceImage.image = UIImage(named: "d4-1")
+            }
+            else if named == "D6" {
+                self?.diceImage.image = UIImage(named: "d6-1")
+            }
+            else if named == "D8" {
+                self?.diceImage.image = UIImage(named: "d8-1")
+            }
+            else if named == "D10" {
+                self?.diceImage.image = UIImage(named: "d10-1")
+            }
+            else if named == "D10%" {
+                self?.diceImage.image = UIImage(named: "d10%-1")
+            }
+            else if named == "D12" {
+                self?.diceImage.image = UIImage(named: "d12-1")
+            }
+            else if named == "D20" {
+                self?.diceImage.image = UIImage(named: "d20-1")
+            }
+        })
     }
     
     @objc func tapGeasture(){
         let rolledNumber = Int.random(in: 1...6)
         diceImage.image = UIImage(named: "d6-\(rolledNumber)")
-        
+
         UIView.transition(with: diceImage, duration: 0.5, options: .transitionCurlDown, animations: nil, completion: nil)
     }
-    
+
 }
-
-
